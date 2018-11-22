c2b5e71db6a4465210d8ff304028b9ddb23d9cb9
Ventislav Yotov <>
2018-11-22 13:20:50 +0100

    Test endDateShouldBeAfterBeginDate

--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -1,5 +1,32 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
+
+import org.junit.Assert;
 import org.junit.Test;
+/*
+•	The end date should be after the begin date, otherwise a CourseDateException is thrown.
+
+ */
 public class CourseTest {
+
+    @Test
+    public void endDateShouldBeAfterBeginDate() throws CourseDateException, ParseException {
+        SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM/yyyy");
+        Date startDate = dateformat.parse("15/09/2018");
+        Date endDate = dateformat.parse("15/11/2018");
+
+        boolean expect = true;
+        boolean actual = false;
+        Course course = new Course();
+        course.setStartDate(startDate);
+        course.setEndDate(endDate);
+
+        if (course.getEndDate().after(course.getStartDate)){
+            actual = true;
+        }
+
+        Assert.assertEquals("The end date is after the begin date",expect,actual);
+    }
 
 }
